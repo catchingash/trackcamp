844788e30c94b22ee19e35320c61f9ea6d8179d4
Ashley Watkins <AWatkins31@gmail.com>
2015-10-14 16:23:40 -0700

    Fixes my `"` to `'`
    'cause what was I thinking?!

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   # OAuth Login
-  get "/auth/:provider", to: "sessions#create", as: "login"
-  get "/auth/:provider/callback", to: "sessions#create"
+  get '/auth/:provider', to: 'sessions#create', as: 'login'
+  get '/auth/:provider/callback', to: 'sessions#create'
   if Rails.env.development?
     # this is required for the OmniAuth Developer Strategy
-    post "/auth/:provider/callback", to: "sessions#create"
+    post '/auth/:provider/callback', to: 'sessions#create'
   end
 
 end
