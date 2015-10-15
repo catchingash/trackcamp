766f11716fc4710755d86b9135b6706fc2d770fe
Ashley Watkins <AWatkins31@gmail.com>
2015-10-14 17:24:32 -0700

    Adds a param to the Google Oauth call.
    This is required in order to get a refresh token every time a user logs in.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development?
   provider :google_oauth2, ENV["GOOGLE_OAUTH_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
     {
-      scope: 'profile, https://www.googleapis.com/auth/fitness.activity.read, https://www.googleapis.com/auth/fitness.activity.write'
+      scope: 'profile, https://www.googleapis.com/auth/fitness.activity.read, https://www.googleapis.com/auth/fitness.activity.write',
+      prompt: 'consent'
     }
 end
