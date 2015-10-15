4f86af0813e8416466544202af315078890c9bba
Ashley Watkins <AWatkins31@gmail.com>
2015-10-15 16:00:11 -0700

    Fix zquestz/omniauth-google-oauth2#197
    :(

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development?
   provider :google_oauth2, ENV["GOOGLE_OAUTH_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
     {
-      scope: 'profile, https://www.googleapis.com/auth/fitness.activity.read, https://www.googleapis.com/auth/fitness.activity.write',
+      skip_jwt: true, # This makes me upset.
+                      # However, this is required because sometimes the web token check fails
+                      # and throws JWT::InvalidIatError, which is a problem.
+                      # I'm not sure why it fails sometimes and not others.
+                      # This appears to be an issue with the gem.
+                      # See: zquestz/omniauth-google-oauth2#197
+      scope: 'email, https://www.googleapis.com/auth/fitness.activity.read, https://www.googleapis.com/auth/fitness.activity.write',
       prompt: 'consent'
     }
 end
